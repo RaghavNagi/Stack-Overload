f787c0beefbf3e49487395b14883a19a3fc6540b
LOSTSOUL2104 <godofwar210405@gmail.com>
2025-08-18 12:44:42 +0530

    middleware,reply,tag

---
+++ config/passport.js
@@ -0,0 +1,23 @@
+const passport = require("passport");
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const User = require("../models/User");
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    console.log("JWT payload:", jwt_payload); 
+    try {
+      const user = await User.findById(jwt_payload.id).select("-password");
+      if (user) return done(null, user);
+      return done(null, false);
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+module.exports = passport;
---
+++ controllers/replyController.js
@@ -0,0 +1,43 @@
+const Reply = require("../models/Reply");
+
+exports.addReply = async (req, res, next) => {
+  try {
+    const { thread, parent, body } = req.body;
+    const reply = await Reply.create({
+      thread,
+      parent: parent || null,
+      body,
+      author: req.user._id,
+    });
+    res.status(201).json(reply);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getReplies = async (req, res, next) => {
+  try {
+    const { threadId } = req.params;
+    const replies = await Reply.find({ thread: threadId }).populate(
+      "author",
+      "username"
+    );
+    res.json(replies);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.voteReply = async (req, res, next) => {
+  try {
+    const { vote } = req.body; // vote: 1 or -1
+    const reply = await Reply.findById(req.params.id);
+    if (!reply) return res.status(404).json({ message: "Reply not found" });
+
+    reply.votes += vote;
+    await reply.save();
+    res.json({ votes: reply.votes });
+  } catch (err) {
+    next(err);
+  }
+};
